d64977b043367451f270641bdd915b0e1f3211d1
blackholeearth <blackholeearth@users.noreply.github.com>
2025-10-21 17:04:22 +0300

    Update getStatusColor.kt

--- app/src/main/java/com/axiel7/moelist/ui/composables/media/getStatusColor.kt
+++ app/src/main/java/com/axiel7/moelist/ui/composables/media/getStatusColor.kt
@@ -5,11 +5,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.axiel7.moelist.data.model.media.ListStatus
 
-val StatusWatching = Color(0xFF2DB039)
+val StatusWatching = Color(0xFF357f3d)
 val StatusPlanned = Color(0xFFC3C3C3)
-val StatusCompleted = Color(0xFF26448f)
-val StatusOnHold = Color(0xFFf1c83e)
-val StatusDropped = Color(0xFFa12f31)
+val StatusCompleted = Color(0xFF2b406f)
+val StatusOnHold = Color(0xFFc89a1c)
+val StatusDropped = Color(0xFF812d2b)
 
 @Composable
 fun getStatusColor(status: ListStatus?): Color {
